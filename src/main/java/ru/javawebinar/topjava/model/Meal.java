6a622cc7b9342b3848a830f2a52c54f10eea487f
Евгений <gorby777@yandex.ru>
2017-04-29 09:31:53 +0300

    HW0

---
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -0,0 +1,48 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * GKislin
+ * 11.01.2015.
+ */
+
+public class Meal {
+
+
+    private final LocalDateTime dateTime;
+
+
+    private final String description;
+
+    private final int calories;
+
+
+    public Meal(LocalDateTime dateTime, String description, int calories) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public LocalDate getDate() {
+        return dateTime.toLocalDate();
+    }
+
+    public LocalTime getTime() {
+        return dateTime.toLocalTime();
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
